d7846c1eef5cba81a8ce15954746b4017ec211c3
Leandro Gabriel <45115495+VelocityIsNotSpeed@users.noreply.github.com>
2019-06-14 20:00:46 -0300

    Adicionei camera

--- Jogo42-CB-Ray/main.c
+++ Jogo42-CB-Ray/main.c
@@ -7,7 +7,7 @@
 #include "raylib.h"
 #include "raymath.h"
 
-/// Se v for vetor nulo, retorna v, caso contrario retorna v normalizado
+// Se v for vetor nulo, retorna v, caso contrario retorna v normalizado
 Vector2 V2Norm(Vector2 v)
 {
     return (Vector2Length(v) == 0) ? Vector2Zero() : Vector2Normalize(v);
@@ -17,22 +17,27 @@ Vector2 V2Norm(Vector2 v)
 int main(void)
 {
     /// Initialization [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[
+    ///Janela==================================================================
     const int screenWidth = 1280, screenHeight = 720;
     InitWindow(screenWidth, screenHeight, "Teste Jogo42 Raylib");
     SetTargetFPS(60);
-
-    Vector2 playerPos = {300.0, 300.0}; // Posicao do jogador
-    Vector2 playerMoveTo; // Vetor de movimento do jogador
+    ///Player==================================================================
+    Vector2 playerPos = {300.0, 300.0}; // Posicao world do jogador
+    Vector2 playerMoveTo;
     float moveSpeed = 150.0; // Velocidade de movimento (por segundo)
-
+    ///Level===================================================================
     Rectangle obstaculo = {100.0, 100.0, 150.0, 100.0};
+    ///Camera==================================================================
+    Camera2D cam = {0};
+    cam.zoom = 1.0f;
     /// Initialization ]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
 
     // Main game loop
     while (!WindowShouldClose()) // Detect window close button or ESC key
     {
         /// Update [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[
-        // Movimento do player
+        ///Movimento do player=================================================
+        // playerMoveTo armazena a posicao futura do player ignorando colisoes
         playerMoveTo = Vector2Zero();
 
         if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT))
@@ -44,11 +49,11 @@ int main(void)
         if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN))
             playerMoveTo.y += 1.0;
 
-        // Normalizar vetor:
+        // Normalizar vetor
         playerMoveTo = V2Norm(playerMoveTo);
-        // Deixar do tamanho certo:
+        // Deixar do tamanho certo
         playerMoveTo = Vector2Scale(playerMoveTo, moveSpeed * GetFrameTime());
-        // Transformar para referencial global:
+        // Transformar para referencial world
         playerMoveTo = Vector2Add(playerPos, playerMoveTo);
 
         // Se nao houver colisao com obstaculo
@@ -56,33 +61,29 @@ int main(void)
             // Mover player
             playerPos = playerMoveTo;
         }
-
-
-
-        ///--------------------------------------------------------------------
-        // Something
+        ///Mover Camera========================================================
+        //cam.offset.x = 200.0;
+        ///Mouse world position:
+        //TODO: mouse world position
         /// Update ]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
 
         /// Draw [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[
         BeginDrawing();
             ClearBackground(DARKBROWN); // Pintar tudo
-            ///----------------------------------------------------------------
-            // Circulo que representa o jogador
-            DrawCircleGradient(playerPos.x, playerPos.y, 30, SKYBLUE, BLUE);
-            ///----------------------------------------------------------------
-            // Indicador de direção:
-            // Posicao do indicador de direção
-            Vector2 p2m = Vector2Subtract(GetMousePosition(), playerPos);
-            p2m = V2Norm(p2m);
-            p2m = Vector2Scale(p2m, 20);
-            p2m = Vector2Add(playerPos, p2m);
-            // Desenhar indicador de direcao
-            DrawCircleV(p2m, 3, WHITE);
-            ///----------------------------------------------------------------
-            // Obstáculo
-            DrawRectangleRec(obstaculo, GRAY);
-            ///----------------------------------------------------------------
-            // FPS:
+            BeginMode2D(cam);
+                ///Jogador=====================================================
+                // Circulo que representa o jogador
+                DrawCircleGradient(playerPos.x, playerPos.y, 30, SKYBLUE, BLUE);
+                ///Indicador de direcao----------------------------------------
+                Vector2 p2m = Vector2Subtract(GetMousePosition(), playerPos);
+                p2m = Vector2Scale(V2Norm(p2m), 20);
+                p2m = Vector2Add(playerPos, p2m);
+                DrawCircleV(p2m, 3, WHITE);
+                ///Obstaculo===================================================
+                DrawRectangleRec(obstaculo, GRAY);
+            EndMode2D();
+            ///================================================================
+            // FPS
             DrawFPS(10, 10);
 
         EndDrawing();
