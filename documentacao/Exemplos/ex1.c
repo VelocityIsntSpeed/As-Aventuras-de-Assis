4594547361ea32064c29d52e62348d09f1d923e8
Leandro Gabriel <45115495+VelocityIsNotSpeed@users.noreply.github.com>
2019-06-15 23:52:47 -0300

    Muitas mudanças

--- documentacao/Exemplos/ex1.c
+++ documentacao/Exemplos/ex1.c
@@ -6,7 +6,17 @@ NAO COLOQUE LETRAS COM ACENTOS NO CODIGO-FONTE!
 O GITHUB DESKTOP TEM UM BUG COM ISSO QUE FAZ COM QUE OS COMMITS DEEM ERRO.
 ===============================================================================
 
+Nesse projeto de CodeBlocks, cada exemplo tem seu build target. Vc pode mudar
+o target atual no menu drop-down da toolbar de compilador.
+(veja mudar-build-target.png)
+
+===============================================================================
+
 Exemplo 1
+
+Novidades:
+- Colisao com os obstaculos
+- MovePlayer()
 
 ******************************************************************************/
 
@@ -15,6 +25,9 @@ Exemplo 1
 
 #define MOVESPEED 150.0 // Velocidade de movimento do player (por segundo)
 #define PLR_RADIUS 30.0 // Raio do circulo do player
+
+// Move o player
+void MovePlayer(Vector2* playerPos, Rectangle obRet, Vector2 obCircCentro, float raio);
 
 int main(void)
 {
@@ -28,8 +41,10 @@ int main(void)
     // Posicao do jogador
     Vector2 playerPos = {300.0, 300.0};
 
-    // Obstaculo cinza para bloquear o player
-    Rectangle obstaculo = {100.0, 100.0, 150.0, 100.0};
+    // Obstaculos
+    Rectangle obCinza = {100.0, 100.0, 150.0, 100.0};
+    Vector2 obRoxoCentro = {650.0, 300.0};
+    float obRoxoRaio = 100.0;
 
     /// [[[[[ End Initalization ]]]]]
 
@@ -37,55 +52,37 @@ int main(void)
     while (!WindowShouldClose()) // Detect window close button or ESC key
     {
         /// [[[[[ Update ]]]]]
-        ///Movimento do player=================================================
-        /* A posicao para a qual vamos mover o player nesse frame,
-           a partir da posicao atual do player */
-        Vector2 playerMoveTo = Vector2Zero();
+        ///Movimentacao do player==============================================
+        MovePlayer(&playerPos, obCinza, obRoxoCentro, obRoxoRaio);
 
-        if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT))   playerMoveTo.x -= 1.0;
-        if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT))  playerMoveTo.x += 1.0;
-        if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP))     playerMoveTo.y -= 1.0;
-        if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN))   playerMoveTo.y += 1.0;
+        ///Mover obstaculos====================================================
+        if (IsKeyPressed(KEY_SPACE)) {
+            obRoxoCentro.x -= 10.0;
+            obRoxoRaio -= 2.0;
 
-        /* Do jeito que playerMoveTo estah agora, o player anda mais rapido se
-           estiver andando na diagonal. Por exemplo: segurando D e S,
-           playerMoveTo eh {1.0, 1.0}, e a magnitude desse vetor eh ~1.41.
-           Por isso normalizaremos o vetor: */
-        // Nao tem como normalizar o vetor nulo, por isso temos que checar:
-        if (Vector2Length(playerMoveTo) != 0) {
-            playerMoveTo = Vector2Normalize(playerMoveTo);
+            obCinza.x += 10.0f;
+            obCinza.height += 5.0f;
         }
-
-        /* Agora que temos um vetor com a direcao para a qual o player estah
-           tentando andar e de magnitude 1 ou 0, escalamos ele para ficar com a
-           magnitude certa (i.e. a distancia que deveremos andar em 1 frame) */
-        playerMoveTo = Vector2Scale(playerMoveTo, MOVESPEED * GetFrameTime());
-
-        /* Transformar de coordenadas player para coordenadas world (i.e. antes
-           era a partir da posicao atual do player, agora vai ser a partir da
-           origem do world, assim como eh playerPos) */
-        playerMoveTo = Vector2Add(playerPos, playerMoveTo);
-
-        /* Checar se a na posicao playerMoveTo o player estara colidindo com o
-        obstaculo (i.e. interseccionando-o). */
-        if (CheckCollisionCircleRec(playerMoveTo, PLR_RADIUS, obstaculo)) {
-            playerMoveTo = playerPos; // Assim o player nao se moverah
-        }
-
-        // Finalmente, atualizar a posicao do player
-        playerPos = playerMoveTo;
-
         /// [[[[[ End Update ]]]]]
 
         /// [[[[[ Draw ]]]]]
         BeginDrawing();
             // Pintar tudo (para formar o background)
             ClearBackground(DARKBROWN);
+
+            // Obstaculo Roxo
+            DrawCircleV(obRoxoCentro, obRoxoRaio, VIOLET);
+
             // Player
             DrawCircleGradient(playerPos.x, playerPos.y,
                                PLR_RADIUS, SKYBLUE, BLUE);
-            // Obstaculo
-            DrawRectangleRec(obstaculo, GRAY);
+
+            // Obstaculo Cinza
+            DrawRectangleRec(obCinza, GRAY);
+
+            // Controles
+            DrawText("Controles: WASD/Setas e Espaco", 200, 10, 19, MAROON);
+
             // FPS
             DrawFPS(10, 10);
         EndDrawing();
@@ -97,3 +94,33 @@ int main(void)
     CloseWindow(); // Close window and OpenGL context
     return 0;
 }
+
+
+void MovePlayer(Vector2* playerPos, Rectangle obRect, Vector2 obCircCenter, float raio)
+{
+    // Posicao do player no proximo frame em relacao ah posicao atual
+    Vector2 playerMoveTo = Vector2Zero();
+
+    if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT))   playerMoveTo.x -= 1.0;
+    if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT))  playerMoveTo.x += 1.0;
+    if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP))     playerMoveTo.y -= 1.0;
+    if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN))   playerMoveTo.y += 1.0;
+
+    playerMoveTo = Vector2Scale(playerMoveTo, MOVESPEED * GetFrameTime());
+
+    // Transformar para coordenadas world
+    playerMoveTo = Vector2Add(*playerPos, playerMoveTo);
+
+    // Verificar colisão
+    if (!CheckCollisionCircleRec(playerMoveTo, PLR_RADIUS, obRect) &&
+        !CheckCollisionCircles(playerMoveTo, PLR_RADIUS, obCircCenter, raio))
+    {
+        // Atualizar a posicao do player
+        *playerPos = playerMoveTo;
+    }
+}
+
+
+
+
+
