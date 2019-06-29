9c0e269028d5f8f17cb857ea5a02cdf25593fda7
Leandro Gabriel <45115495+VelocityIsNotSpeed@users.noreply.github.com>
2019-06-29 00:31:44 -0300

    Mover o código de desenhar para o próprio arquivo (#11)
    Ver comentário da PR #11 para mais informações.

---
+++ Jogo42-CB-Ray/draw.c
@@ -0,0 +1,73 @@
+/******************************************************************************
+===============================================================================
+                                 ATENCAO!!!
+
+NAO COLOQUE LETRAS COM ACENTOS NO CODIGO-FONTE!
+O GITHUB DESKTOP TEM UM BUG COM ISSO QUE FAZ COM QUE OS COMMITS DEEM ERRO.
+===============================================================================
+******************************************************************************/
+
+#include "raylib.h"
+#include "raymath.h"
+#include "jogo42.h"
+
+// Desenha o texto que diz os controles
+static void DesenharControles()
+{
+    // Texto que vai aparecer
+    const char texto[] =
+    "Controles:\n"
+    "WASD/Setas para andar\n"
+    "Espaco para movimentar obstaculos";
+
+    // Calcular a altura:
+    float alturaTexto = MeasureTextEx(GetFontDefault(), texto, 20, 2).y;
+
+    // Desenhar texto
+    DrawText(texto, 10, GetScreenHeight() - alturaTexto - 10, 20, BLACK);
+}
+
+static void DesenharJogador(GameState* gs, Texture2D* spriteJog)
+{
+    // A parte da textura a ser utilizada
+    const Rectangle SRC_REC = {0, 0, spriteJog->width, spriteJog->height};
+
+    // Posicao e tamanho na tela
+    const Rectangle DEST_REC = {gs->posJog.x, gs->posJog.y,\
+                                RAIO_JOG * 2, RAIO_JOG * 2};
+
+    // Posicao do centro relativa ah parte superior esquerda do DEST_REC
+    const Vector2 CENTRO = {RAIO_JOG, RAIO_JOG};
+
+    DrawTexturePro(*spriteJog,\
+                   SRC_REC, DEST_REC, CENTRO, gs->rotJog, WHITE);
+}
+
+// Desenha tudo
+void Desenhar(GameState* gs, Texture2D* spriteJog)
+{
+    // Pintar tudo (para formar o background)
+    ClearBackground(DARKBROWN);
+
+    // Obstaculo circular
+    DrawCircleV(gs->obstCircCentro, gs->obstCircRaio,
+                gs->obstCircTaAndando ? PURPLE : VIOLET);
+
+    //Jogador
+    DesenharJogador(gs, spriteJog);
+
+    // Obstaculo retangular
+    DrawRectangleRec(gs->obstRet, GRAY);
+
+    // Controles
+    DesenharControles();
+
+    // Texto com raio do obstaculo
+    DrawText(TextFormat("Raio = %.1f", gs->obstCircRaio),
+             gs->obstCircCentro.x, gs->obstCircCentro.y, 20, WHITE);
+
+    // FPS
+    DrawFPS(10, 10);
+}
+
+
--- Jogo42-CB-Ray/jogo42.h
+++ Jogo42-CB-Ray/jogo42.h
@@ -1,8 +1,24 @@
+/******************************************************************************
+===============================================================================
+                                 ATENCAO!!!
+
+NAO COLOQUE LETRAS COM ACENTOS NO CODIGO-FONTE!
+O GITHUB DESKTOP TEM UM BUG COM ISSO QUE FAZ COM QUE OS COMMITS DEEM ERRO.
+===============================================================================
+******************************************************************************/
 #ifndef JOGO42_H_INCLUDED
 #define JOGO42_H_INCLUDED
 
 #include "raylib.h"
 #include "raymath.h"
+
+// Velocidade do jogador (por segundo)
+#define VEL_JOG 150.0f
+// Raio do jogador
+#define RAIO_JOG 30.0f
+
+// Velocidade do obstaculo circular (por segundo)
+#define VEL_CIRC 40.0f
 
 // Struct de estado do jogo
 typedef struct {
@@ -20,6 +36,27 @@ typedef struct {
     bool obstCircTaAndando;
 } GameState;
 
-// TODO: helper functions and macros
+///Declaracao de funcoes definidas em outros arquivos==========================
+// draw.c
+void Desenhar(GameState* gs, Texture2D* spriteJog);
+
+/* Exemplos:
+// arquivo-que-ainda-nao-existe.c
+void FuncaoQueAindaNaoExiste();
+void FuncaoQueAindaNaoExiste();
+void FuncaoQueAindaNaoExiste();
+
+// outro-arquivo-que-ainda-nao-existe.c
+void FuncaoQueAindaNaoExiste();
+void FuncaoQueAindaNaoExiste();
+*/
+
+///Funcoes helper (i.e. utilitarias, declaradas e definidas aqui)
+// Ainda nao existe nenhuma
 
 #endif // JOGO42_H_INCLUDED
+
+
+
+
+
